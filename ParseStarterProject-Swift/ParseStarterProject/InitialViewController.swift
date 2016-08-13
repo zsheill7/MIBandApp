c471e6acbb8369a529e9ac4f24c5be904056d167
zsheill7 <zsheill7@gmail.com>
2016-08-12 21:29:15 -0700

    commit

--- ParseStarterProject-Swift/ParseStarterProject/InitialViewController.swift
+++ ParseStarterProject-Swift/ParseStarterProject/InitialViewController.swift
@@ -33,7 +33,7 @@ class InitialViewController: UIViewController {
         let navigationViewController = initialViewController.viewControllers![3] as! UINavigationController
         let photosViewController = navigationViewController.topViewController as! PhotosViewController
         photosViewController.store = PhotoStore()
-        nonmemberStoryboard.instantiateViewControllerWithIdentifier("nonmemberTBC")
+        presentViewController(initialViewController, animated: true, completion: nil)
         
     }
     

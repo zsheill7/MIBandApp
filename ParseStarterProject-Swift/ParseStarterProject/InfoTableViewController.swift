8c6baf28173c08e9c8de9d7757efb0d2736564ef
zsheill7 <zsheill7@gmail.com>
2016-09-04 21:25:26 -0700

    Finished new info view controller

---
+++ ParseStarterProject-Swift/ParseStarterProject/InfoTableViewController.swift
@@ -0,0 +1,19 @@
+//
+//  InfoTableViewController.swift
+//  MI Band
+//
+//  Created by Zoe on 9/4/16.
+//  Copyright © 2016 Parse. All rights reserved.
+//
+
+import UIKit
+
+class InfoTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.tableView.backgroundColor = UIColor.whiteColor()
+    }
+    
+}
